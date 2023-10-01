c2f94c4b6ccad01d4243b3006fd1edd2f8e79471
Alfredo Sánchez <alfred3ks@gmail.com>
2023-10-01 12:24:30 +0200

    Creo el modelo de productos.

---
+++ models/Product.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    onSale: {
+      type: Boolean,
+      default: false,
+    },
+    price: {
+      type: Number,
+      required: true,
+      index: true,
+    },
+    photo: {
+      type: String,
+    },
+    tags: {
+      type: [String],
+      index: true,
+    },
+  },
+  // control the pluralization of mongoose
+  { collection: 'products' }
+);
+
+// Create a model from the schematic
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
