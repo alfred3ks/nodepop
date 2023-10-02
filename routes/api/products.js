118defbd1a16fbc61c185b6ad42e22e057441a2f
Alfredo Sánchez <alfred3ks@gmail.com>
2023-10-02 11:10:36 +0200

    Agrego los metodos de la api para buscar por id, actualizar por id, y borrar un producto.

--- routes/api/products.js
+++ routes/api/products.js
@@ -15,6 +15,19 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+// GET: /api/products(_id)
+// Get product by id
+// http://localhost:3000/api/products/(_id)
+router.get('/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const product = await Product.findById(id);
+    res.json({ result: [{ product }] });
+  } catch (err) {
+    next(err);
+  }
+});
+
 // POST: /api/products/
 // Create a product:
 // http://localhost:3000/api/products
@@ -27,6 +40,35 @@ router.post('/', async (req, res, next) => {
     const productSaved = await product.save();
 
     res.json({ results: { productSaved } });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// PUT: /api/products(_id)
+// Update product
+// http://localhost:3000/api/products/(_id)
+router.put('/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const data = req.body;
+    const updateProduct = await Product.findByIdAndUpdate(id, data, {
+      new: true,
+    });
+    res.json({ result: [{ updateProduct }] });
+  } catch (err) {
+    next(err);
+  }
+});
+
+//DELETE: /api/products/(_id)
+// Delete product
+// http://localhost:3000/api/products/(_id)
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    await Product.deleteOne({ _id: id });
+    res.json();
   } catch (err) {
     next(err);
   }
